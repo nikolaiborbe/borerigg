3ab46a697e2aa50d52a99f361eedf28f4fb973bb
Nikolai Grytvik Borbe <nikolaiborbe@gmail.com>
2026-02-26 15:21:44 +0100

    short to run program

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ import RPi.GPIO as GPIO
 from servo_controller import ServoController
 import time
 
-servo = ServoController(32)
-servo.set_pos(0)
-servo.set_pos(180)
-servo.set_pos(90)
-servo.set_pos(45)
-servo.set_pos(30)
-servo.set_pos(20)
+TRIGGER_PIN = 36
 
-servo.cleanup()
+def main():
+
+    while GPIO.input(TRIGGER_PIN) == GPIO.HIGH:
+        time.sleep(0.05)
+
+    servo = ServoController(32)
+
+    servo.set_pos(0)
+    servo.set_pos(180)
+    servo.set_pos(90)
+    servo.set_pos(45)
+    servo.set_pos(30)
+    servo.set_pos(20)
+
+    servo.cleanup()
+
+
+if __name__ == "__main__": 
+    main()
